efe480daf77306f32ad8bf03a79f7fe8a9a41c2e
Nikhil Arora <nikhil290192@gmail.com>
2021-06-05 12:34:50 +0530

    Initial Project Setup

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by NiKhil AroRa on 05/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
