97edd40f95e920f726165f5ff6815131b3ff0ac1
Nikhil Arora <nikhil290192@gmail.com>
2021-06-06 23:45:31 +0530

    Progress

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🏎", "🚐", "🚗", "🚌"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(emojis, id: \.self) { emoji in
+                CardView(content: emoji)
+            }
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 12 Pro Max")
+        ContentView()
+            .preferredColorScheme(.light)
+            .previewDevice("iPhone 12 Pro Max")
     }
 }
